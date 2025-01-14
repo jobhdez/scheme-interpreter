01302208f932f86949a66857e7afe661bb44fdd2
lara254 <lara2993@proton.me>
2025-01-14 09:48:05 -0800

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -17,7 +17,7 @@ from parser import (
 
 import operator as op
 
-# `Env` and `Procedure were taken from Peter Norvig's Scheme interpreter
+# `Env` and `Procedure` and `standard_env` were taken from Peter Norvig's Scheme interpreter
 class Env(dict):
     def __init__(self, params=(), args=(), outer=None):
         self.update(zip(params, args))
